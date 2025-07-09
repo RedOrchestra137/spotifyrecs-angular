6b5e05d8412905fa1d3a4c6de781c9d6dd3d6d38
RedOrchestra137 <rstimpy137@gmail.com>
2025-07-09 17:25:06 +0200

    v

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // scans all HTML and TypeScript files in src folder
+    "./src/**/*.{html,ts,tsx,js,jsx}",
+    "./src/app/**/*.{html,ts}",
+    "./projects/**/*.{html,ts}" // if using Angular workspace
   ],
   theme: {
     extend: {},
