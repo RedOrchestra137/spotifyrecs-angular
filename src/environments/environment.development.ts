6bc51c41b11aeebb809efb8a4eb39ad50309e44e
RedOrchestra137 <rstimpy137@gmail.com>
2025-07-30 22:02:03 +0200

    v

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/',
+    apiUrl: 'http://188.245.56.199:3000/',
     streamServer:'http://188.245.56.199:3000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api-winter-orchid.fly.dev/',
+    apiUrl: 'http://188.245.56.199:3000/',
     streamServer:'http://188.245.56.199:3000/'
 };
