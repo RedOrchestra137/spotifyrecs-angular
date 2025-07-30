d75ca240774cb9344d7e9e4353a092ffd38ac272
RedOrchestra137 <rstimpy137@gmail.com>
2025-07-30 22:18:30 +0200

    v

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://188.245.56.199:3000/',
-    streamServer:'https://188.245.56.199:3000/'
+    apiUrl: 'http://188.245.56.199:3000/',
+    streamServer:'http://188.245.56.199:3000/'
 };
